0f9140b327d2d1ab008fde177deabfdbca31f63d
Handan Zhang <hmix.cc@gmail.com>
2021-03-01 20:16:22 +0100

    adding testing file

---
+++ fundamental-concepts.rb
@@ -0,0 +1,19 @@
+Concepts:
+utility
+risk aversion
+
+Compounding and discounting
+
+FV(T) = PV(1+r)^T
+
+PV = FV(T)/(1+r)^T
+
+r = [T squaroot(FV(T)/PV)] - 1
+
+
+
+# Valuing levered projects
+WACC, the weighted average cost of capital
+
+WACC = re * (E/V) + rd * (1 - tau) * D/V
+
